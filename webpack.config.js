3cb121209be37486d7f034dae64cc7a74a386f28
Kevin Ostafinski <kevinostafinski@gmail.com>
2018-10-18 17:31:45 +0200

    initial boilerplate structure that handles babel, html and sass

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        include: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'src/styles/main'),
+        ],
+        use: [
+          {
+            loader: 'style-loader'
+          }, 
+          {
+            loader: 'css-loader'
+          }, 
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
