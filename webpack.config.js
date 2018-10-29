0f1b8c7dff35ac248255f40ddaa07eab63c50601
Kevin Ostafinski <kevinostafinski@gmail.com>
2018-10-29 18:13:37 +0100

    added file-loader for custom fonts support

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,17 @@ module.exports = {
             loader: 'sass-loader'
           }
         ]
-      }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+    }
     ]
   },
   plugins: [
